35d7fa60b47c28078a51f1c4990da18887541df4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-15 22:26:00 +0000

    init

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAYRkcOLJJPbvJQyMu6ZRn7P_O3Tpl7Vpk",
+            authDomain: "housify-ujjxfr.firebaseapp.com",
+            projectId: "housify-ujjxfr",
+            storageBucket: "housify-ujjxfr.appspot.com",
+            messagingSenderId: "626917610268",
+            appId: "1:626917610268:web:aa8ba5e5d2248a751b20d9"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
---
+++ lib/index.dart
@@ -0,0 +1,11 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/auth_home/auth_home_widget.dart' show AuthHomeWidget;
+export '/auth_welcome_screen/auth_welcome_screen_widget.dart'
+    show AuthWelcomeScreenWidget;
+export '/account_profile_creation/auth_2_create/auth2_create_widget.dart'
+    show Auth2CreateWidget;
+export '/account_profile_creation/auth_2_login/auth2_login_widget.dart'
+    show Auth2LoginWidget;
+export '/account_profile_creation/auth_2_forgot_password/auth2_forgot_password_widget.dart'
+    show Auth2ForgotPasswordWidget;
